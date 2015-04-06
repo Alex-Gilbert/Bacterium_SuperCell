6b6f8b0e5d4ab4d15ad973f629f7047cb6e4b891
simeonovzhan <simeonovzhan@gmail.com>
2015-04-06 00:58:41 -0500

    Ok here is some script for the Burrower to make it move. Avoids collisions with Plane. Change it to whatever the name for the final projects floor is. Also saved the scene for the Bomber.

---
+++ Assets/_Scripts/Enemy AI/Burrower.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burrower : MonoBehaviour {
+	private GameObject Player;
+	private float distance;
+
+
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindWithTag("Player");
+
+	}
+	public void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.name == "Plane")
+			Physics.IgnoreCollision(this.GetComponent<Collider>(),collision.gameObject.GetComponent<Collider>());	
+
+	}
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance (Player.transform.position, transform.position);
+		if ((distance < 15) && (distance > 6)) {
+
+			Vector3 player_position = Player.transform.position;
+			player_position.y = -4.0f;
+			transform.LookAt(player_position);
+			transform.position = Vector3.MoveTowards (transform.position, player_position, 4f * Time.deltaTime);
+		} else if (distance < 10) {
+			transform.LookAt(Player.transform.position);
+			transform.position = Vector3.MoveTowards (transform.position, Player.transform.position, 4f * Time.deltaTime);
+		}
+	}
+	
+}
