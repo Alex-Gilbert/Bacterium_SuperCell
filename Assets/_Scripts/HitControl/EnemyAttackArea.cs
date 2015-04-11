80b835a329d06f16a140915b358710fd3a7d6d47
Alex-Gilbert <alexander.paul.gilbert@gmail.com>
2015-04-10 23:09:56 -0500

    Added Steph's SkyBox
    This game is looking sick

---
+++ Assets/_Scripts/HitControl/EnemyAttackArea.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttackArea : MonoBehaviour 
+{
+    GameObject player;
+
+    public PlayerAttack attackActive;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            if (attackActive != null)
+            {
+                ActivateHittables.HitAll(other.gameObject, attackActive);
+            }
+            else
+            {
+                player = other.gameObject;
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            player = null;
+        }
+    }
+
+    public void ActivateAttack(PlayerAttack attack)
+    {
+        attackActive = attack;
+
+        if(player != null)
+        {
+            ActivateHittables.HitAll(player, attackActive);
+        }
+    }
+
+    public void EndAttack()
+    {
+        attackActive = null;
+    }
+}
