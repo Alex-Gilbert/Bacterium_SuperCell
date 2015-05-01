41cedee78eb0cac8822e12144c453372405fea91
Alex-Gilbert <alexander.paul.gilbert@gmail.com>
2015-05-01 10:20:04 -0500

    Added the plants

---
+++ Assets/_Scripts/Utilities/EmitPulsate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmitPulsate : MonoBehaviour 
+{
+
+    Renderer render;
+    Color emissionColor;
+    public float frequency = 1f;
+    public float amplitude = 1f;
+    public float baseMult = 0.75f;
+
+    // Use this for initialization
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+        emissionColor = render.material.GetColor("_EmissionColor") * baseMult * amplitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (render.enabled)
+        {
+            float glow = (2 + Mathf.Cos(Time.time * frequency)) * amplitude;
+            render.material.SetColor("_EmissionColor", emissionColor * glow);
+        }
+    }
+}
